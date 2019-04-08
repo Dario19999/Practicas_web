f52bb4eb01d833cb09e25b91c19cbedd4dc80960
Dario <dario_gow3@hotmail.com>
2019-04-08 00:28:24 -0500

    query de bd_practica agregado

---
+++ ejemploxml 1 y 2/platillos.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-04-2019 a las 04:16:06
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `platillos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `platillos`
+--
+
+CREATE TABLE `platillos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `precio` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `platillos`
+--
+
+INSERT INTO `platillos` (`id`, `nombre`, `precio`) VALUES
+(1, 'Sopa du macaco', 100),
+(2, 'sandwich', 50),
+(3, 'queso', 20);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `platillos`
+--
+ALTER TABLE `platillos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `platillos`
+--
+ALTER TABLE `platillos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
